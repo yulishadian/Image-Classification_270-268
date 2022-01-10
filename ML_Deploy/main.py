32a9a43a72e290003adc740b0fb79a2c2beb6e6b
Eka Yunia Primastuti <ekayunnia@webmail.umm.ac.id>
2022-01-10 21:33:14 +0700

    deploy

---
+++ ML_Deploy/main.py
@@ -0,0 +1,68 @@
+import time
+import os
+import cv2
+import numpy as np
+from PIL import Image
+from flask import Flask, request, redirect, render_template
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+@app.route("/")
+def index():
+    return render_template('/select.html', )
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    chosen_model = request.form['select_model']
+    model_dict = {'CNN_js' :   ['static/MLModule/CNN.json','static/MLModule/CNN.h5'],
+                  'model_js'    :   ['static/MLModule/model.json','static/MLModule/model.h5'],
+                  'VGG16'       :   'static/MLModule/VGG16.h5',
+                  'InceptionV3' :   'static/MLModule/InceptionV3.h5'}
+
+    if chosen_model in model_dict:
+        if "_js" in chosen_model:
+            json_file = open(model_dict[chosen_model][0], 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            model = model_from_json(loaded_model_json)
+            model.load_weights(model_dict[chosen_model][1])
+        else:
+            model = load_model(model_dict[chosen_model]) 
+    else:
+        model = load_model(model_dict[0])
+
+    file = request.files["file"]
+    file.save(os.path.join('static', 'temp.jpg'))
+    img = cv2.cvtColor(np.array(Image.open(file)), cv2.COLOR_BGR2RGB)
+    img = np.expand_dims(cv2.resize(img, model.layers[0].input_shape[0][1:3] if not model.layers[0].input_shape[1:3] else model.layers[0].input_shape[1:3]).astype('float32') / 255, axis=0)
+    start = time.time()
+    pred = model.predict(img)[0]
+    labels = (pred > 0.5).astype(np.int)
+    print(labels)
+    runtimes = round(time.time()-start,4)
+    respon_model = [round(elem * 100, 2) for elem in pred]
+    return predict_result(chosen_model, runtimes, respon_model, 'temp.jpg')
+
+def predict_result(model, run_time, probs, img):
+    class_list = {'Daisy': 0, 'Dandelion': 1, 'Rose': 2, 'Sunflower': 3, 'Tulip': 4}
+    idx_pred = probs.index(max(probs))
+    labels = list(class_list.keys())
+    return render_template('/result_select.html', labels=labels, 
+                            probs=probs, model=model, pred=idx_pred, 
+                            run_time=run_time, img=img)
+
+if __name__ == "__main__": 
+        app.run(debug=True, host='0.0.0.0', port=2000)
